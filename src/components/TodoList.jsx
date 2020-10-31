46c6beec0caa20c8baff42b2277c8f43facb5d1b
Varun kumar <45607763+kickvire@users.noreply.github.com>
2020-10-31 19:29:16 +0530

    change

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,6 +6,9 @@ export default function TodoList(props) {
       <button className="delete" onClick={() => props.onDelete(props.id)}>
         Delete
       </button>
+      <button className="edit" onClick={() => props.onEdit(props.id)}>
+        Edit
+      </button>
     </>
   );
 }
