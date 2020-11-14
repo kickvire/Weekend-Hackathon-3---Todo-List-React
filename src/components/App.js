3f75bc63b12508b4842027bb5c69fe094905686a
Varun kumar <45607763+kickvire@users.noreply.github.com>
2020-11-14 18:13:36 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,51 @@
-import React from "react";
-import "./../styles/App.css";
-import TodoList from "./TodoList";
+import React, { useState } from "react";
+import "./styles.css";
 
-function App() {
-  const [todos, setTodos] = React.useState([]);
-  const [input, setInput] = React.useState("");
+export default function App() {
+  const [inputText, setInputText] = useState("");
+  const [listOfitems, setlistOfitems] = useState([]);
+  const [save, setsaveButton] = useState("");
 
-  const handleChange = ({ target }) => {
-    setInput(target.value);
-    console.log(input);
+  const handleChange = (event) => {
+    let newInput = event.target.value;
+    setInputText(newInput);
   };
-  const addTodo = (event) => {
-    setTodos([...todos, input]);
-    console.log(todos);
-    setInput("");
+  const handleAdd = () => {
+    if (inputText === "") return;
+    setlistOfitems((previtems) => [...previtems, inputText]);
+    setInputText("");
   };
-
   const handleDelete = (id) => {
-    const filterTodos = todos.filter((todo, index) => index !== id);
-    setTodos(filterTodos);
+    const restAfterremove = listOfitems.filter((todo, index) => index !== id);
+    setlistOfitems(restAfterremove);
   };
+
+  const handleEdit = (id, item) => {
+    setInputText(item);
+    setsaveButton(id);
+  };
+
+  const handleSave = () => {
+    if (inputText === "") return;
+    listOfitems[save] = inputText;
+    setInputText("");
+    setsaveButton("");
+  };
+
   return (
     <div id="main">
-      <textarea id="task" value={input} onChange={handleChange} />
-      <button id="btn" disabled={!input} onClick={addTodo}>
-        Add Todo
-      </button>
-      {todos.map((todo, index) => (
-        <TodoList onDelete={handleDelete} id={index} text={todo} />
-      ))}
+      <input type="text" onChange={handleChange} value={inputText} />
+      <button onClick={handleAdd}>Add</button>
+      <button onClick={handleSave}>Save</button>
+      <ol>
+        {listOfitems.map((item, index) => (
+          <li>
+            {item}
+            <button onClick={() => handleDelete(index, item)}>Delete</button>
+            <button onClick={() => handleEdit(index, item)}>Edit</button>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
-
-export default App;
